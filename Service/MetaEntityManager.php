a40c17a8d3d31a7f362afca2ed91c9c14f1c38a6
joris <joris@2le.net>
2017-11-28 16:04:24 +0100

    ajout des method ds les fields

--- Service/MetaEntityManager.php
+++ Service/MetaEntityManager.php
@@ -17,7 +17,7 @@ class MetaEntityManager
         $this->em = $em;
     }
 
-    public function generateFields($className, array $columns = null){
+    public function generateFields($className, array $columns = null, $withoutMethodsFields = false){
         $return = [];
         if(is_array($columns)) {
             foreach ($columns as $column) {
@@ -37,6 +37,19 @@ class MetaEntityManager
             }
         }
 
+        if(!$withoutMethodsFields) {
+            foreach ($reflectionClass->getMethods() as $k => $m) {
+                foreach ($r->getMethodAnnotations($m) as $annotation) {
+                    $name = lcfirst(substr($m->getName(), 3));
+                    if (get_class($annotation) == Annotation\Field::class and ($columns == null or in_array($name, $columns))) {
+                        /* @var Annotation\Field $field */
+                        $field = $annotation;
+                        $field->setName($name);
+                        $return[$name] = $field;
+                    }
+                }
+            }
+        }
         $trashcan = [];
         foreach($return as $k => $field){
             if(!$field) $trashcan[] = $k;
@@ -48,7 +61,7 @@ class MetaEntityManager
     public function generateFormFields($className, array $columns = null){
         $return = [];
 
-        $fields = $this->generateFields($className);
+        $fields = $this->generateFields($className,null, true);
         $r = new AnnotationReader();
         $reflectionClass = new \ReflectionClass($className);
         $entityForm = $r->getClassAnnotation($reflectionClass, Annotation\EntityForm::class);
